a99fe145cfaa13e30641cbe1e7365af60df4a5c7
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-10-08 23:38:15 +0330

    add express  in node

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('app working')
+import express from 'express';
+import './example';
+
+export const app = express();
+
+app.listen(3000);
+
+
+app.get('/', (req, res, next) => {
+    res.send('Welcome to node app')
+})
+
