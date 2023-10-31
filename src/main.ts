879a7a858b3103577440e231f639edd23d5a1694
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-10-31 21:55:59 +0330

    install typeorm and connect to the database by datasource

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import express, {Express} from 'express';
+import 'reflect-metadata';
 import dotenv from 'dotenv';
+import {createDataSource} from "./db/data-source";
+
 const cors = require('cors')
 export const app = express();
 
@@ -11,17 +14,28 @@ function configENV() {
     dotenv.config();
 }
 
-function runService() {
+function connectToDatabase() {
+    console.log('connecting to the database...')
+    return createDataSource().initialize()
+}
+
+async function startService() {
+    /** set env **/
     configENV();
-    const port = process.env['PORT'];
-    const appName = process.env['APP_NAME'];
+    const [port, appName] = [process.env['PORT'], process.env['APP_NAME']];
+    /** apply middlewares **/
     applyMiddlewares(app);
-    app.listen(port, () => {
-        console.log('App successfully started on port: ' + port)
+    /** connecting to the database **/
+    await connectToDatabase()
+        .then(() => console.log('connected to the database successfully!'))
+        .catch(() => console.log('connection to database is failed!!'));
+    /** listening  **/
+    await app.listen(port, () => {
+        console.log(`[${appName}] App successfully started on port: [${port}] `)
     });
     app.get('/', (req, res, next) => {
         res.send(`Welcome to ${appName} App`)
     })
 }
 
-// runService();
+startService();
